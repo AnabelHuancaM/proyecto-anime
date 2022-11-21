28aca2fc734baffcfaac838c3273ea695f8e92ed
valentino17-0 <82229577+valentino17-0@users.noreply.github.com>
2022-11-20 20:47:32 -0500

    3

--- src/main/java/com/certus/spring/controller/HomeController.java
+++ src/main/java/com/certus/spring/controller/HomeController.java
@@ -1,16 +1,21 @@
 package com.certus.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app") //indicando ruta inicial
 public class HomeController {
 	
-	@RequestMapping("/home") //indicando ruta inicial
+	@GetMapping("/home")  //indicando las rutas que llamaremos
 	public String HolaMundo() {
 		return "Home";
 	}
 
-	
+	@GetMapping("/home2")  //indicando las rutas que llamaremos
+	public String HolaMundo2() {
+		return "Home2";
+	}
 	//@GetMapping("/home", "/")
 }
