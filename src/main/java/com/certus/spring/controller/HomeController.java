770c9f7babe10c7ec589a94d6917c5022887f525
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2022-11-20 20:00:40 -0500

    controller

--- src/main/java/com/certus/spring/controller/HomeController.java
+++ src/main/java/com/certus/spring/controller/HomeController.java
@@ -1,5 +1,11 @@
 package com.certus.spring.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/app") //indicando ruta inicial
+public class HomeController {
+	
+	//@GetMapping({"/home", "/"})
 }
