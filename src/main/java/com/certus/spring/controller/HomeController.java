962aa954d2eb3d4647cdda72cd2cc00513cc2206
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2022-11-20 21:12:07 -0500

    home 2

--- src/main/java/com/certus/spring/controller/HomeController.java
+++ src/main/java/com/certus/spring/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
 		return "Home";
 	}
 
-	@GetMapping("/home2")          //indicando las rutas que llamaremos
+	@GetMapping({"/home2", "/anabel", "Home2"})          //indicando las rutas que llamaremos
 	public String HolaMundo2() {
 		return "Home2";
 	}
