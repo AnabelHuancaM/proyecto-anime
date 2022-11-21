1aa959cc605919905d987b60a72001fffa826afa
valentino17-0 <82229577+valentino17-0@users.noreply.github.com>
2022-11-20 22:29:47 -0500

    añadiendo lista mascota

--- src/main/java/com/certus/spring/controller/HomeController.java
+++ src/main/java/com/certus/spring/controller/HomeController.java
@@ -1,10 +1,15 @@
 package com.certus.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.certus.spring.models.Mascota;
 
 @Controller
 @RequestMapping("/app") //indicando ruta inicial
@@ -16,8 +21,23 @@ public class HomeController {
 	
 	@GetMapping({"/home1", "/valentino", "/Home1"})            //indicando las rutas que llamaremos
 	public String HolaMundo(Model model) {
-		model.addAttribute("TituloPagina", titlePage);
 		
+		
+		Mascota mascota1 = new Mascota();
+		
+		mascota1.setNombre("Manchita");
+		mascota1.setTipo("Gato");
+		mascota1.setRaza("Atigrado");
+		mascota1.setColor("Blanco con manchas medias plomas");
+		mascota1.setEdad("1 año");
+			
+		List<Mascota> listita1 = new ArrayList<>();
+		listita1.add(mascota1);
+		
+		
+		model.addAttribute("TituloPagina", titlePage);
+		model.addAttribute("titulo1", "Esta es la lista de mascotas");
+		model.addAttribute("listita1", listita1);
 		return "Home";
 	}
 
