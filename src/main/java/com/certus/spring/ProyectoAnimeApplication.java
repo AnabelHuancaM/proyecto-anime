40236a611ae77602f80b27899c46bc3f39d57c81
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2022-11-20 19:40:08 -0500

    subiendo archivos

---
+++ src/main/java/com/certus/spring/ProyectoAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.certus.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certus/spring/ProyectoAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certus.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
