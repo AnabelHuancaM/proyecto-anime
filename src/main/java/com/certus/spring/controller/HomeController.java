9decee70e66fa59a5ec7279623a1cb23fc206d41
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2022-11-20 22:30:33 -0500

    actualizando model

--- src/main/java/com/certus/spring/controller/HomeController.java
+++ src/main/java/com/certus/spring/controller/HomeController.java
@@ -1,10 +1,15 @@
 package com.certus.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.certus.spring.models.Personaje;
 
 @Controller
 @RequestMapping("/app") //indicando ruta inicial
@@ -25,6 +30,27 @@ public class HomeController {
 	public String HolaMundo2(Model model) {
 		
 		model.addAttribute("TituloPagina", titlePage);
+		
+		//creando personaje 1
+		Personaje personaje = new Personaje();
+		
+		//determinando nombre del personaje(atributo del model personaje)
+		personaje.setNombre("Ace");
+		personaje.setEdad("14");
+		personaje.setSerie("One Piece");
+		personaje.setEstado("Morido");
+		personaje.setNacimiento("14-10-22");
+		
+		
+		List<Personaje> listita2 = new ArrayList<>();
+		listita2.add(personaje); //añadiendo personaje 1
+		
+		model.addAttribute("tituloPagina", titlePage);
+		model.addAttribute("titulo2", "Lista de personajes");
+		model.addAttribute("listita2", listita2);
+		
+		
+		
 		return "Home2";
 	}
 
--- src/main/java/com/certus/spring/models/Personaje.java
+++ src/main/java/com/certus/spring/models/Personaje.java
@@ -1,5 +1,42 @@
 package com.certus.spring.models;
 
 public class Personaje {
+	private String nombre;
+	private String edad;
+	private String serie;
+	private String estado;
+	private String nacimiento;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEdad() {
+		return edad;
+	}
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+	public String getSerie() {
+		return serie;
+	}
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getNacimiento() {
+		return nacimiento;
+	}
+	public void setNacimiento(String nacimiento) {
+		this.nacimiento = nacimiento;
+	}
+
 
 }
