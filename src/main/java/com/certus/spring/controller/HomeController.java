ed9cd10b9ab503ff70f6b5f54273ea1581e4589e
valentino17-0 <82229577+valentino17-0@users.noreply.github.com>
2022-11-20 21:48:52 -0500

    titulohome1

--- src/main/java/com/certus/spring/controller/HomeController.java
+++ src/main/java/com/certus/spring/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.certus.spring.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/app") //indicando ruta inicial
 public class HomeController {
 	
-	@Value("titulo.Generado ")            //evaluando el titulo del propertis
-	private String tituloPagina;
+	@Value("${title.generic}")            //evaluando el titulo del propertis
+	private String titlePage;
 	
 	
 	@GetMapping({"/home1", "/valentino", "/Home1"})            //indicando las rutas que llamaremos
-	public String HolaMundo() {
+	public String HolaMundo(Model model) {
+		model.addAttribute("TituloPagina", titlePage);
+		
 		return "Home";
 	}
 
